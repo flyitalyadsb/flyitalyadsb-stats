eb7396810ac1af10cd5c811002fa938080b136be
flyitalyadsb <admin@flyitalyadsb.com>
2022-08-25 18:42:26 +0200

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-TMP=/tmp/flyitalyadsb-stats-git
+TMP=/tmp/graphs1090
 if ! command -v git; then
     apt-get update
     apt-get install -y git
 fi
 rm -rf "$TMP"
 set -e
+git clone https://github.com/wiedehopf/graphs1090.git "$TMP"
+echo "Installing Graphs1090"
+bash install.sh
+TMP=/tmp/flyitalyadsb-stats-git
+rm -rf "$TMP"
+set -e
 git clone https://github.com/flyitalyadsb/flyitalyadsb-stats.git "$TMP"
 cd "$TMP"
+echo "Installing Stats Package"
 bash install.sh
